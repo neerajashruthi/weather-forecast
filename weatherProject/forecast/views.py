7df3422092c608ed6b8ba1ca98131c0eb02a10b7
neerajashruthi <shruthi.mneeraja1@gmail.com>
2026-03-19 23:50:00 +0530

    updated weather view

--- weatherProject/forecast/views.py
+++ weatherProject/forecast/views.py
@@ -172,125 +172,125 @@ def predit_future(model, current_value):
 def weather_view(request):
     if request.method == 'POST':
         city = request.POST.get('city')
-        if city =='':
-             return render(request, 'weather.html', {            
-            'day_stats_flag': 'false'
+        
+        current_weather = get_current_weather(city)
+        if current_weather is None:
+            return render(request, "weather.html", {
+                "error": "City not found"
             })
+        wind_deg = current_weather["wind_gust_dir"] % 360
+
+        compass_points = [("N", 348.75, 360),("N", 0, 11.25),("NNE", 11.25, 33.75),("NE", 33.75, 56.25),("ENE", 56.25, 78.75),("E", 78.75, 101.25),
+                        ("ESE", 101.25, 123.75),("SE", 123.75, 146.25),("SSE", 146.25, 168.75),("S", 168.75, 191.25),("SSW", 191.25, 213.75),
+                        ("SW", 213.75, 236.25),("WSW", 236.25, 258.75),("W", 258.75, 281.25),("WNW", 281.25, 303.75),("NW", 303.75, 326.25),
+                        ("NNW", 326.25, 348.75),
+                        ]
+
+        compass_direction = next(
+        point for point, start, end in compass_points
+        if start <= wind_deg < end
+        )
+
+        compass_direction_encoded = (
+        le.transform([compass_direction])[0]
+        if compass_direction in le.classes_
+        else -1)
+
+        current_data = {
+            "MinTemp":current_weather["temp_min"],
+            "MaxTemp":current_weather["temp_max"],
+            "WindGustDir":compass_direction_encoded,
+            "WindGustSpeed": current_weather["WindGustSpeed"],
+            "Humidity": current_weather["humidity"],
+            "Pressure":current_weather["pressure"],
+            "Temp":current_weather["current_temp"],
+        }
+        current_df = pd.DataFrame([current_data])
+        rain_prediction = rain_model.predict(current_df)[0]
+        future_temp = predit_future(temp_model,current_weather["temp_min"])
+        future_humidity = predit_future(hum_model,current_weather["humidity"])
+        timezone = pytz.timezone('Asia/Kolkata')
+        now = datetime.now(timezone)
+        next_hour = now + timedelta(hours=1)
+        next_hour = next_hour.replace(minute = 0, second = 0, microsecond = 0)
+        future_times = [(next_hour + timedelta(hours=i)).strftime("%H:00") for i in range(5)]
+
+        time1, time2, time3, time4, time5 = future_times
+        temp1, temp2, temp3, temp4, temp5 = future_temp
+        hum1, hum2, hum3, hum4, hum5 = future_humidity
+        
+        pressure_text = ''
+        wind_text=''
+        day_stats_flag ='false'
+
+        raw_description = current_weather["description"].lower()
+
+        if "rain" in raw_description:
+            weather_class = "rain"
+            day_stats_flag ='true'
+        elif "cloud" in raw_description:
+            weather_class = "clouds"
+            day_stats_flag ='true'
+        elif "clear" in raw_description:
+            weather_class = "clear"
+            day_stats_flag ='true'
+        elif "mist" in raw_description or "haze" in raw_description:
+            weather_class = "mist"
+            day_stats_flag ='true'
         else:
-            current_weather = get_current_weather(city)
-            if current_weather is None:
-                return render(request, "weather.html", {
-                    "error": "City not found"
-                })
-            wind_deg = current_weather["wind_gust_dir"] % 360
+            weather_class = "clear"
 
-            compass_points = [("N", 348.75, 360),("N", 0, 11.25),("NNE", 11.25, 33.75),("NE", 33.75, 56.25),("ENE", 56.25, 78.75),("E", 78.75, 101.25),
-                            ("ESE", 101.25, 123.75),("SE", 123.75, 146.25),("SSE", 146.25, 168.75),("S", 168.75, 191.25),("SSW", 191.25, 213.75),
-                            ("SW", 213.75, 236.25),("WSW", 236.25, 258.75),("W", 258.75, 281.25),("WNW", 281.25, 303.75),("NW", 303.75, 326.25),
-                            ("NNW", 326.25, 348.75),
-                            ]
-
-            compass_direction = next(
-            point for point, start, end in compass_points
-            if start <= wind_deg < end
-            )
-
-            compass_direction_encoded = (
-            le.transform([compass_direction])[0]
-            if compass_direction in le.classes_
-            else -1)
-
-            current_data = {
-                "MinTemp":current_weather["temp_min"],
-                "MaxTemp":current_weather["temp_max"],
-                "WindGustDir":compass_direction_encoded,
-                "WindGustSpeed": current_weather["WindGustSpeed"],
-                "Humidity": current_weather["humidity"],
-                "Pressure":current_weather["pressure"],
-                "Temp":current_weather["current_temp"],
-            }
-            current_df = pd.DataFrame([current_data])
-            rain_prediction = rain_model.predict(current_df)[0]
-            future_temp = predit_future(temp_model,current_weather["temp_min"])
-            future_humidity = predit_future(hum_model,current_weather["humidity"])
-            timezone = pytz.timezone('Asia/Kolkata')
-            now = datetime.now(timezone)
-            next_hour = now + timedelta(hours=1)
-            next_hour = next_hour.replace(minute = 0, second = 0, microsecond = 0)
-            future_times = [(next_hour + timedelta(hours=i)).strftime("%H:00") for i in range(5)]
-
-            time1, time2, time3, time4, time5 = future_times
-            temp1, temp2, temp3, temp4, temp5 = future_temp
-            hum1, hum2, hum3, hum4, hum5 = future_humidity
             
-            pressure_text = ''
-            wind_text=''
-            day_stats_flag ='false'
-
-            raw_description = current_weather["description"].lower()
-
-            if "rain" in raw_description:
-                weather_class = "rain"
-                day_stats_flag ='true'
-            elif "cloud" in raw_description:
-                weather_class = "clouds"
-                day_stats_flag ='true'
-            elif "clear" in raw_description:
-                weather_class = "clear"
-                day_stats_flag ='true'
-            elif "mist" in raw_description or "haze" in raw_description:
-                weather_class = "mist"
-                day_stats_flag ='true'
-            else:
-                weather_class = "clear"
-
-                
+        
+        context = {
+            'location': city,
+            'current_temp': current_weather['current_temp'],
+            'MinTemp': current_weather['temp_min'],
+            'MaxTemp': current_weather['temp_max'],
+            'feels_like': current_weather['feels_like'],
+            'humidity': current_weather['humidity'],
+            'clouds' : current_weather['clouds'],
+            'weather_class': weather_class,
+            'real_description': current_weather['description'],
+            'city': current_weather['city'],
+            'country': current_weather['country'],
             
-            context = {
-                'location': city,
-                'current_temp': current_weather['current_temp'],
-                'MinTemp': current_weather['temp_min'],
-                'MaxTemp': current_weather['temp_max'],
-                'feels_like': current_weather['feels_like'],
-                'humidity': current_weather['humidity'],
-                'clouds' : current_weather['clouds'],
-                'weather_class': weather_class,
-                'real_description': current_weather['description'],
-                'city': current_weather['city'],
-                'country': current_weather['country'],
-                
 
-                'time': datetime.now(),
-                'date': datetime.now().strftime("%B, %d, %Y"),
-                'wind': current_weather['WindGustSpeed'],
-                'pressure': current_weather['pressure'],
-                'visibility': current_weather['Visibility'],
-                'time1':time1,
-                'time2':time2,
-                'time3':time3,
-                'time4':time4,
-                'time5':time5,
+            'time': datetime.now(),
+            'date': datetime.now().strftime("%B, %d, %Y"),
+            'wind': current_weather['WindGustSpeed'],
+            'pressure': current_weather['pressure'],
+            'visibility': current_weather['Visibility'],
+            'time1':time1,
+            'time2':time2,
+            'time3':time3,
+            'time4':time4,
+            'time5':time5,
 
-                'temp1':f"{round(temp1,1)}",
-                'temp2':f"{round(temp2,1)}",
-                'temp3':f"{round(temp3,1)}",
-                'temp4':f"{round(temp4,1)}",
-                'temp5':f"{round(temp5,1)}",
+            'temp1':f"{round(temp1,1)}",
+            'temp2':f"{round(temp2,1)}",
+            'temp3':f"{round(temp3,1)}",
+            'temp4':f"{round(temp4,1)}",
+            'temp5':f"{round(temp5,1)}",
 
-                'hum1': f"{round(hum1,1)}",
-                'hum2': f"{round(hum2,1)}",
-                'hum3': f"{round(hum3,1)}",
-                'hum4': f"{round(hum4,1)}",
-                'hum5': f"{round(hum5,1)}",
-                'wind_text':'kilometers per hour. pressure is ',
-                'pressure_text' :'mb. visibility is ',
-                'visibility_text':'. Maximum temperature is ',
-                'maxtemp_text':'℃. Minimum temperature is ',
-                'mintemp_text':'℃.', 'day_stats_flag':day_stats_flag
-            }
-            return render(request, 'weather.html', context)
-    return render(request, 'weather.html', {
-    'weather_class': 'clouds',
-    'real_description': 'Search for a city',
-    'day_stats_flag': 'false'
-    })
+            'hum1': f"{round(hum1,1)}",
+            'hum2': f"{round(hum2,1)}",
+            'hum3': f"{round(hum3,1)}",
+            'hum4': f"{round(hum4,1)}",
+            'hum5': f"{round(hum5,1)}",
+            'wind_text':'kilometers per hour. pressure is ',
+            'pressure_text' :'mb. visibility is ',
+            'visibility_text':'. Maximum temperature is ',
+            'maxtemp_text':'℃. Minimum temperature is ',
+            'mintemp_text':'℃.', 'day_stats_flag':day_stats_flag
+        }
+        return render(request, 'weather.html', context)
+        return render(request, 'weather.html', {
+        'weather_class': 'clouds',
+        'real_description': 'Search for a city',
+        'day_stats_flag': 'false'
+        })
+    else :return render(request, 'weather.html', {        
+        'day_stats_flag': 'false'
+        })
+    
