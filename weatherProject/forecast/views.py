e8810cf1168d64853c5803318eaa28498f7e7746
neerajashruthi <shruthi.mneeraja1@gmail.com>
2026-03-20 00:38:55 +0530

    updated weather view

--- weatherProject/forecast/views.py
+++ weatherProject/forecast/views.py
@@ -236,7 +236,7 @@ def weather_view(request):
         else:
             weather_class = "clear"
 
-        if(city==''):
+        if city=='':
             day_stats_flag ='false'
         
         
