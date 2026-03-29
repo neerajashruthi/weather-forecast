35ae15d7f20b215dc1386f2fda79a2a1e2ed1563
neerajashruthi <shruthi.mneeraja1@gmail.com>
2026-03-29 20:11:21 +0530

    updated weather view

--- weatherProject/forecast/views.py
+++ weatherProject/forecast/views.py
@@ -166,8 +166,8 @@ def predit_future(model, current_value):
 def weather_view(request):
     if request.method == 'POST':
         city = request.POST.get('city')
-        cliettimezone= request.get('utcoffset'),
-        clienttimezoneoffset =request.get('offset')
+        cliettimezone = request.POST.get('utcoffset'),
+        clienttimezoneoffset = request.POST.get('offset')
         print('test value : ' ,city)
         current_weather = get_current_weather(city)
         if current_weather is None:
